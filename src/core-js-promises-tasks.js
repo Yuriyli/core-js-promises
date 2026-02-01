99324cd524478242c67d162f43a190195e3fea50
Yuriy Li <0yuriyli@gmail.com>
2026-02-01 17:24:21 +0300

    feat: add solution for getAllResult()

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -113,8 +113,14 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const wrappedPromises = promises.map((promise) =>
+    Promise.resolve(promise)
+      .then((value) => value)
+      .catch(() => null)
+  );
+
+  return Promise.all(wrappedPromises);
 }
 
 /**
